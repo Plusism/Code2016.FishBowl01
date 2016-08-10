4187c94530c3dc3d08805c63da037bbe8fb85638
ce.miyashita <ce.miyashita@live.jp>
2016-08-10 23:52:08 +0900

    ソースコードの公開

---
+++ CodeBehind/Code2016.FishBowl01.UwpApp/App.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Code2016.FishBowl01.UwpApp
+{
+	/// <summary>
+	/// Provides application-specific behavior to supplement the default Application class.
+	/// </summary>
+	sealed partial class App : Application
+	{
+		/// <summary>
+		/// Initializes the singleton application object.  This is the first line of authored code
+		/// executed, and as such is the logical equivalent of main() or WinMain().
+		/// </summary>
+		public App()
+		{
+			this.InitializeComponent();
+			this.Suspending += OnSuspending;
+		}
+
+		/// <summary>
+		/// Invoked when the application is launched normally by the end user.  Other entry points
+		/// will be used such as when the application is launched to open a specific file.
+		/// </summary>
+		/// <param name="e">Details about the launch request and process.</param>
+		protected override void OnLaunched(LaunchActivatedEventArgs e)
+		{
+#if DEBUG
+			if (System.Diagnostics.Debugger.IsAttached)
+			{
+				this.DebugSettings.EnableFrameRateCounter = true;
+			}
+#endif
+
+			// タイトルバー領域までアプリの表示を拡張する
+			var coreTitleBar = Windows.ApplicationModel.Core.CoreApplication.GetCurrentView().TitleBar;
+			coreTitleBar.ExtendViewIntoTitleBar = true;
+			// タイトルバー領域のカラーリングを指定する
+			var appTitleBar = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().TitleBar;
+			appTitleBar.BackgroundColor = Color.FromArgb(0xFF, 0x76, 0xAC, 0x90);
+			appTitleBar.ForegroundColor = Colors.White;
+			appTitleBar.InactiveBackgroundColor = Color.FromArgb(0xFF, 0x76, 0xAC, 0x90);
+			appTitleBar.InactiveForegroundColor = Color.FromArgb(0xff, 0x66, 0x66, 0x66);
+			appTitleBar.ButtonBackgroundColor = Color.FromArgb(0xFF, 0x76, 0xAC, 0x90);
+			appTitleBar.ButtonForegroundColor = Colors.White;
+			appTitleBar.ButtonInactiveBackgroundColor = Color.FromArgb(0xFF, 0xCE, 0xE6, 0xA0);
+			appTitleBar.ButtonInactiveForegroundColor = Color.FromArgb(0xff, 0x34, 0x34, 0x34);
+			appTitleBar.ButtonHoverBackgroundColor = Color.FromArgb(0xff, 0x15, 0x4E, 0x60);
+			appTitleBar.ButtonHoverForegroundColor = Colors.White;
+			appTitleBar.ButtonPressedBackgroundColor = Color.FromArgb(0xff, 0x34, 0x34, 0x34);
+			appTitleBar.ButtonPressedForegroundColor = Colors.White;
+
+			Frame rootFrame = Window.Current.Content as Frame;
+
+			// Do not repeat app initialization when the Window already has content,
+			// just ensure that the window is active
+			if (rootFrame == null)
+			{
+				// Create a Frame to act as the navigation context and navigate to the first page
+				rootFrame = new Frame();
+
+				rootFrame.NavigationFailed += OnNavigationFailed;
+
+				if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+				{
+					//TODO: Load state from previously suspended application
+				}
+
+				// Place the frame in the current Window
+				Window.Current.Content = rootFrame;
+			}
+
+			if (e.PrelaunchActivated == false)
+			{
+				if (rootFrame.Content == null)
+				{
+					// When the navigation stack isn't restored navigate to the first page,
+					// configuring the new page by passing required information as a navigation
+					// parameter
+					rootFrame.Navigate(typeof(MainPage), e.Arguments);
+				}
+				// Ensure the current window is active
+				Window.Current.Activate();
+			}
+		}
+
+		/// <summary>
+		/// Invoked when Navigation to a certain page fails
+		/// </summary>
+		/// <param name="sender">The Frame which failed navigation</param>
+		/// <param name="e">Details about the navigation failure</param>
+		void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+		{
+			throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+		}
+
+		/// <summary>
+		/// Invoked when application execution is being suspended.  Application state is saved
+		/// without knowing whether the application will be terminated or resumed with the contents
+		/// of memory still intact.
+		/// </summary>
+		/// <param name="sender">The source of the suspend request.</param>
+		/// <param name="e">Details about the suspend request.</param>
+		private void OnSuspending(object sender, SuspendingEventArgs e)
+		{
+			var deferral = e.SuspendingOperation.GetDeferral();
+			//TODO: Save application state and stop any background activity
+			deferral.Complete();
+		}
+	}
+}
---
+++ CodeBehind/Code2016.FishBowl01.UwpApp/MainPage.xaml.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace Code2016.FishBowl01.UwpApp
+{
+	/// <summary>
+	/// Code2016 きんぎょばち (1) BGMとSE鳴らすモバイルアプリ
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		public MainPage()
+		{
+			this.InitializeComponent();
+		}
+
+		/// <summary>
+		/// BGM音量
+		/// </summary>
+		public double _volume = 0.5;
+
+		/// <summary>
+		/// BGMボリューム操作時
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void MusicVolume_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+		{
+			if (this.MusicMediaElement == null)
+				return;
+			_volume = this.MusicVolume.Value / 100;
+			this.MusicMediaElement.Volume = _volume;
+		}
+
+		/// <summary>
+		/// BGMミュート操作時
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void MusicMute_Click(object sender, RoutedEventArgs e)
+		{
+			if (this.MusicMediaElement == null)
+				return;
+			var flag = !(this.MusicMute.IsChecked ?? false);
+			this.MusicVolume.IsEnabled = flag;
+			this.MusicPlaybackRate.IsEnabled = flag;
+			this.MusicButton1.IsEnabled = flag;
+			this.MusicButton2.IsEnabled = flag;
+			this.MusicButton3.IsEnabled = flag;
+			this.MusicButton4.IsEnabled = flag;
+			this.MusicMediaElement.Volume = (flag) ? _volume : 0;
+		}
+
+		/// <summary>
+		/// BGM音量(最大)
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void MusicButton1_Click(object sender, RoutedEventArgs e)
+		{
+			if (this.MusicMediaElement == null)
+				return;
+			_volume = 1;
+			this.MusicVolume.Value = _volume * 100;
+			this.MusicMediaElement.Volume = _volume;
+		}
+
+		/// <summary>
+		/// BGM音量(標準)
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void MusicButton2_Click(object sender, RoutedEventArgs e)
+		{
+			if (this.MusicMediaElement == null)
+				return;
+			_volume = 0.5;
+			this.MusicVolume.Value = _volume * 100;
+			this.MusicMediaElement.Volume = _volume;
+		}
+
+		/// <summary>
+		/// BGMフェードアウト(速め)
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private async void MusicButton3_Click(object sender, RoutedEventArgs e)
+		{
+			if (this.MusicMediaElement == null)
+				return;
+			do
+			{
+				_volume -= 0.01;
+				this.MusicVolume.Value = _volume * 100;
+				this.MusicMediaElement.Volume = _volume;
+				await Task.Delay(25);
+			} while (_volume > 0);
+		}
+
+		/// <summary>
+		/// BGMフェードアウト(遅め)
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private async void MusicButton4_Click(object sender, RoutedEventArgs e)
+		{
+			if (this.MusicMediaElement == null)
+				return;
+			do
+			{
+				_volume -= 0.01;
+				this.MusicVolume.Value = _volume * 100;
+				this.MusicMediaElement.Volume = _volume;
+				await Task.Delay(50);
+			} while (_volume > 0);
+		}
+
+		/// <summary>
+		/// BGMを選択時
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void MusicListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			this.MusicMediaElement.Stop();
+			if (this.MusicListBox.SelectedItem == null)
+				return;
+			var tag = (this.MusicListBox.SelectedItem as ListBoxItem).Tag;
+			this.MusicMediaElement.Source = new Uri($"ms-appx:///Assets/Music/{tag}.mp3");
+			this.MusicMediaElement.IsLooping = true;
+			this.MusicMediaElement.Play();
+		}
+
+		/// <summary>
+		/// BGM再生速度を選択時(0.5～3.0)
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void MusicPlaybackRate_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (this.MusicMediaElement == null)
+				return;
+			var item = this.MusicPlaybackRate.SelectedItem as ComboBoxItem;
+			var rate = double.Parse((string)item.Content);
+			this.MusicMediaElement.PlaybackRate = rate;
+		}
+
+		/// <summary>
+		/// BGM停止ボタンをクリック時
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void MusicStop_Click(object sender, RoutedEventArgs e)
+		{
+			this.MusicListBox.SelectedIndex = -1;
+		}
+
+		/// <summary>
+		/// SE再生ボタンをクリック時
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void SoundButton_Click(object sender, RoutedEventArgs e)
+		{
+			var tag = (sender as Button).Tag;
+			this.SoundMediaElement.Source = new Uri($"ms-appx:///Assets/Sound/{tag}.mp3");
+			this.SoundMediaElement.Play();
+		}
+	}
+}
